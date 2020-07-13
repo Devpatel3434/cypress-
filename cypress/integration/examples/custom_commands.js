1f64e0381a467cae428992f77fe4a2fe90d09522
Dev Patel <dev2462000@gmail.com>
2020-07-13 12:55:04 +0530

    Update custom_commands.js

--- cypress/integration/examples/custom_commands.js
+++ cypress/integration/examples/custom_commands.js
@@ -1,5 +1,5 @@
 describe('cust cmd',()=>{
-
+// trying to login in  normal way .
 it('login',()=>{
 
 cy.visit('https://admin-demo.nopcommerce.com/login')
@@ -8,12 +8,13 @@ cy.get('input[name=Password]').clear().type('admin')
 cy.get('[type=submit]').click()
 
 })
-
+// try login using custom command created in command.js under support
 it('login_2',()=>{
 
-    cy.login('admin@yourstore.com','admin')
+    cy.login('admin@yourstore.com','admin') // passing directly value  locater already define in command.js
     
     })
+    //try login using custom command created in command.js under support
     it('login_3',()=>{
 
         cy.login('admin@yourstore.com','admin')
