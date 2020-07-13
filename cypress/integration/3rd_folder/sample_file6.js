a50d41ee98a6d6a683ca919ac9054e5823e2268d
Dev Patel <dev2462000@gmail.com>
2020-07-13 12:57:13 +0530

    Create sample_file6.js

---
+++ cypress/integration/3rd_folder/sample_file6.js
@@ -0,0 +1,11 @@
+describe('hello',()=>{
+
+    it('hi',()=>{
+    
+        cy.visit('https://admin-demo.nopcommerce.com/login') // it will redirect to this url website. 
+    })
+    
+    
+    
+    
+    })
